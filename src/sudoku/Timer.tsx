e69271c3209d222e0c8df8bd30d414536abfbd6f
Axel Vasquez <a.axelvc@gmail.com>
2021-10-23 23:16:33 -0500

    feat(sudoku): :sparkles: add timer logic

--- src/sudoku/Timer.tsx
+++ src/sudoku/Timer.tsx
@@ -1,8 +1,32 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
-// TODO: add timer logic
+const SECOND_IN_MILLISECONDS = 1_000
+const MINUTE_IN_SECONDS = 60
+
+function padTime(time: number): string {
+  return `${time}`.padStart(2, '0')
+}
+
 const Timer: FC = () => {
-  return <button>00:00</button>
+  const [seconds, setSeconds] = useState(0)
+  const [running, setRunning] = useState(false)
+
+  useEffect(() => {
+    if (!running) return
+
+    const id = setInterval(() => setSeconds(secs => secs + 1), SECOND_IN_MILLISECONDS)
+
+    return () => clearInterval(id)
+  }, [running])
+
+  const mins = Math.floor(seconds / MINUTE_IN_SECONDS)
+  const secs = seconds % MINUTE_IN_SECONDS
+
+  return (
+    <button role="timer" onClick={() => setRunning(!running)}>
+      {padTime(mins)}:{padTime(secs)}
+    </button>
+  )
 }
 
 export default Timer
