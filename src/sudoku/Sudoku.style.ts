dbfb74b0058815288931748a066e2fde90d2e065
Axel Vasquez <a.axelvc@gmail.com>
2021-11-05 01:00:08 -0600

    refactor(ui): :lipstick: modify sudoku box to set button as container of the box

--- src/sudoku/Sudoku.style.ts
+++ src/sudoku/Sudoku.style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Header = styled.header`
   display: flex;
@@ -20,34 +20,29 @@ export const SudokuGrid = styled.div`
   border-radius: 0.5rem;
 `
 
-export const Box = styled.div`
+interface BoxProps {
+  hasMarks: boolean
+}
+
+export const Box = styled.button<BoxProps>`
   position: relative;
   aspect-ratio: 1;
-  display: flex;
+  display: grid;
+  place-items: center;
   font-weight: 700;
   color: var(--color-fg);
   background: var(--color-gray-200);
   border-radius: 0.2rem;
-`
-export const BoxValue = styled.button`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`
 
-export const BoxMarks = styled.div`
-  box-sizing: border-box;
-  display: grid;
-  grid-template:
-    'n1 n2 n3' 1fr
-    'n4 n5 n6' 1fr
-    'n7 n8 n9' 1fr / 1fr 1fr 1fr;
-  place-items: center;
-  width: 100%;
-  height: 100%;
-  padding: 10%;
+  ${({ hasMarks }) =>
+    hasMarks &&
+    css`
+      grid-template:
+        'n1 n2 n3' 1fr
+        'n4 n5 n6' 1fr
+        'n7 n8 n9' 1fr / 1fr 1fr 1fr;
+      padding: 10%;
+    `}
 `
 
 interface MarkProps {
--- src/sudoku/Sudoku.tsx
+++ src/sudoku/Sudoku.tsx
@@ -6,7 +6,7 @@ import { fillBox, setSudoku, FillData } from './sudokuSlice'
 
 import DropDown from '../common/DropDown/DropDown'
 import Timer from './Timer'
-import { Box, BoxMarks, BoxValue, Header, Mark, SudokuGrid } from './Sudoku.style'
+import { Box, Header, Mark, SudokuGrid } from './Sudoku.style'
 
 const Sudoku: FC = () => {
   const [difficulty, setDifficulty] = useState('easy')
@@ -44,20 +44,21 @@ const Sudoku: FC = () => {
       <SudokuGrid>
         {puzzle.map((arr, row) =>
           arr.map((box, col) => (
-            <Box key={`${row}${col}`}>
-              <BoxValue
-                disabled={box.blocked}
-                onClick={() => fillBoxWithControlValue({ row, col })}
-              >
-                {Boolean(box.value) && box.value}
-              </BoxValue>
-              <BoxMarks>
-                {box.marks.map(mark => (
+            <Box
+              key={`${row}${col}`}
+              hasMarks={box.marks.length >= 1}
+              disabled={box.blocked}
+              onClick={() => fillBoxWithControlValue({ row, col })}
+            >
+              {box.value ? (
+                <span>{box.value}</span>
+              ) : (
+                box.marks.map(mark => (
                   <Mark key={mark} mark={mark}>
                     {mark}
                   </Mark>
-                ))}
-              </BoxMarks>
+                ))
+              )}
             </Box>
           )),
         )}
