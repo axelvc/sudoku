e89d674f81c26c469bdf8597f66d34b86f75bc97
Axel Vasquez <a.axelvc@gmail.com>
2021-11-27 23:52:44 -0600

    feat(sudoku): :alien: add real api to sudokuService

--- src/sudoku/sudokuService.ts
+++ src/sudoku/sudokuService.ts
@@ -5,39 +5,32 @@ export interface ResponseType {
   solution: Sudoku
 }
 
+const API = 'https://sugoku.herokuapp.com'
+
 export const difficulties = ['easy', 'medium', 'hard']
 
-// TODO: implement real api
+async function getSudoku(difficulty: string): Promise<Sudoku> {
+  const res = await fetch(`${API}/board?difficulty=${difficulty}`)
+  const { board } = await res.json()
+
+  return board
+}
+
+async function getSolution(puzzle: Sudoku): Promise<Sudoku> {
+  const opts = {
+    method: 'POST',
+    body: new URLSearchParams({ board: JSON.stringify(puzzle) }),
+  }
+
+  const res = await fetch(`${API}/solve`, opts)
+  const { solution } = await res.json()
+
+  return solution
+}
+
 export async function fetchSudoku(difficulty: string): Promise<ResponseType> {
-  return await new Promise((resolve, reject) =>
-    setTimeout(
-      // () => reject(new Error('Not allowed')),
-      () =>
-        resolve({
-          puzzle: [
-            [7, 0, 0, 9, 1, 0, 0, 5, 6],
-            [0, 2, 0, 0, 5, 0, 7, 0, 9],
-            [5, 0, 0, 4, 7, 0, 0, 2, 0],
-            [0, 0, 0, 0, 4, 0, 0, 0, 0],
-            [0, 5, 6, 0, 8, 9, 2, 0, 7],
-            [8, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 1, 6, 0, 0, 0, 0, 2],
-            [0, 7, 2, 0, 0, 1, 0, 4, 5],
-            [9, 0, 5, 7, 2, 0, 0, 6, 1],
-          ],
-          solution: [
-            [7, 3, 8, 9, 1, 2, 4, 5, 6],
-            [1, 2, 4, 3, 5, 6, 7, 8, 9],
-            [5, 6, 9, 4, 7, 8, 1, 2, 3],
-            [2, 1, 3, 5, 4, 7, 6, 9, 8],
-            [4, 5, 6, 1, 8, 9, 2, 3, 7],
-            [8, 9, 7, 2, 6, 3, 5, 1, 4],
-            [3, 4, 1, 6, 9, 5, 8, 7, 2],
-            [6, 7, 2, 8, 3, 1, 9, 4, 5],
-            [9, 8, 5, 7, 2, 4, 3, 6, 1],
-          ],
-        }),
-      5000,
-    ),
-  )
+  const puzzle = await getSudoku(difficulty)
+  const solution = await getSolution(puzzle)
+
+  return { puzzle, solution }
 }
